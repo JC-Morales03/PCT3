02bfccb6dca41929a939d97f99982e6ed4fa6a0b
JC-Morales03 <juan_morales8@usmp.pe>
2024-05-25 15:01:55 -0500

    integración Listar Usuarios

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using pc3.Integration;
 using PCT3.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,8 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<ListarUsuariosApiIntegration, ListarUsuariosApiIntegration>();
 
 var app = builder.Build();
 
